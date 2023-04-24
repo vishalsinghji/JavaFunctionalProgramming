c75602bb20d4fc9ae92636b4b1393f9b19b76704
vishalsinghji <vishalkmr272@gmail.com>
2023-04-24 11:12:08 +0530

    Added Predefined FI

---
+++ src/PreDefinedFI/ConsumerPractice.java
@@ -0,0 +1,30 @@
+package PreDefinedFI;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerPractice {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(34,67,8,23,67,89,90);
+
+        Consumer<Integer> consumer = System.out :: println ;
+
+        consumer.accept(56);
+
+        printElements(list, consumer);
+
+    }
+
+    private static <T> void printElements(List<T> list, Consumer<T> consumer) {
+
+        for (T t : list) {
+
+            consumer.accept(t);
+
+        }
+
+    }
+
+}
